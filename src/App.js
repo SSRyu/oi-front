bfafbeb633a7f8239b603c8a7ad204988766f7d7
SSRyu <jjkztok3@gmail.com>
2021-06-19 15:29:11 +0900

    Hello world를 We are adniel로 수정

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           {/* Edit <code>src/App.js</code> and save to reload. */}
-          Hello World!
+          We are adniel!
         </p>
         <a
           className="App-link"
