a64c3aa76af10a1e923c9895a070b808f9ad32fc
SSRyu <jjkztok3@gmail.com>
2021-06-26 16:48:57 +0900

    Add layout.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Header from './components/layout/header';
+import Footer from './components/layout/footer';
+import Clock from './components/clock';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Header />
+    <Clock />
+    <Footer />
   </React.StrictMode>,
   document.getElementById('root')
 );
